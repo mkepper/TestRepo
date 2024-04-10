b1fe8cddfdb24a56e5bcc265a3085d26645d0cd5
mkepper <mkepperkepper@gmail.com>
2024-04-10 09:35:21 -0500

    Initial commit
    Yeah, it's mandatory

---
+++ VeryCreative/VeryCreative/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VeryCreative
+//
+//  Created by KEPLEY, MATTHEW O. on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VeryCreative/VeryCreative/VeryCreativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeryCreativeApp.swift
+//  VeryCreative
+//
+//  Created by KEPLEY, MATTHEW O. on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VeryCreativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
